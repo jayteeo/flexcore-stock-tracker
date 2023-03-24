9c903d4b5098c00d29b1357a7e340d8464fd4962
Michael <michaelmcadow@gmail.com>
2023-03-24 06:17:47 -0400

    added models

---
+++ backend/models/Cores.js
@@ -0,0 +1,16 @@
+import mongoose from 'mongoose';
+
+const Schema = mongoose.Schema;
+
+const coresSchema = new Schema ({
+    size: {
+        type: Number,
+        required: true,
+    },
+    count: {
+        type: Number,
+        required: true,
+    },
+}, { timestamps: true })
+
+export default mongoose.model('Cores', coresSchema);
--- backend/routes/cores.js
+++ backend/routes/cores.js
@@ -1,8 +1,25 @@
 import express from 'express';
+import Cores from '../models/Cores.js';
 
 const router = express.Router();
 
 //GET all cores
+router.get('/', (req, res) => {
+    res.json({mssg: 'GET all cores'})
+});
+
+//GET single core
+router.get('/:id', (req, res) => {
+    res.json({mssg: 'GET all cores'})
+});
+
+//POST a core
+router.post('/', (req, res) => {
+    const {size, count} = req.body;
+    res.json({mssg: 'GET all cores'})
+});
+
+//DELETE A core
 router.get('/', (req, res) => {
     res.json({mssg: 'GET all cores'})
 });
