5aae99a9d0bfc0c1a9fc8ee8de077e50faf56f0a
Michael <michaelmcadow@gmail.com>
2023-03-31 06:40:54 -0400

    bridging front and backend

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -1,7 +1,29 @@
+import { useEffect, useState } from "react";
+
+
 const Home = () => {
+    const [cores, setCores] = useState(null)
+    
+    useEffect(() => {
+        const fetchCores = async () => {
+            const response = await fetch('/api/cores');
+            const json = await response.json();
+
+            if (response.ok) {
+                setCores(json);
+            }
+        }
+
+        fetchCores();
+    }, [])
+
     return (
         <div className="home">
-            <h2>Home</h2>
+            <div className="cores">
+                {cores && cores.map((core) => (
+                    <p key={core._id}>{cores.size}</p>
+                ))}
+            </div>
         </div>
     )
 }
